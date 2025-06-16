f347f610d6536f3d85565cc65b24ec94389a977d
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-06-16 12:41:12 +0200

    articles mock

---
+++ src/data/articles.jsx
@@ -0,0 +1,18 @@
+export const articles = [
+  {
+    id: 1,
+    title: "Un café et une idée",
+    date: "2025-06-15T10:00:00",
+    image: "/article1.jpg",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel semper metus. Sed tincidunt tortor non enim vulputate, nec bibendum nunc aliquam. Aenean pretium rutrum orci.",
+  },
+  {
+    id: 2,
+    title: "Petite pause, grande pensée",
+    date: "2025-06-14T09:45:00",
+    image: "/article2.jpg",
+    content:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
+  },
+];
