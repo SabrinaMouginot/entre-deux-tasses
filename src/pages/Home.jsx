998ae8f955843cff9feafead591bbab9dd40c471
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-06-16 12:33:49 +0200

    Footer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Article from "../components/Article";
+import Footer from "../components/Footer";
+
+export default function Home() {
+
+    return (
+        <>
+            <Header />
+            <main></main>
+            <Footer />
+        </>
+    );
+}
