fed2d8bc4ae269bf59e4c4f9bb26d82da1610e32
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-06-20 02:49:49 +0200

    decrementation des reactions ( precedemment seul l incrémentation etait faite )

--- src/components/Reactions.jsx
+++ src/components/Reactions.jsx
@@ -1,32 +1,54 @@
 import { useState } from "react";
 
 export default function Reactions() {
-  const [reactions, setReactions] = useState({
-    "👍": 0,
-    "👏": 0,
-    "🤝": 0,
-    "❤️": 0,
-    "💡": 0,
-    "😄": 0,
-  });
+  const emojis = ["👍", "👏", "🤝", "❤️", "💡", "😄"];
+
+  const [reactions, setReactions] = useState(
+  //   {
+  //   "👍": 0,
+  //   "👏": 0,
+  //   "🤝": 0,
+  //   "❤️": 0,
+  //   "💡": 0,
+  //   "😄": 0,
+  // }
+      emojis.reduce((acc, emoji) => ({ ...acc, [emoji]: 0 }), {})
+);
+
+  const [clicked, setClicked] = useState(
+    emojis.reduce((acc, emoji) => ({ ...acc, [emoji]: false }), {})
+  );
 
   const handleReact = (emoji) => {
+        const alreadyClicked = clicked[emoji];
+
     setReactions((prev) => ({
       ...prev,
-      [emoji]: prev[emoji] + 1,
+      [emoji]: prev[emoji] + 
+      // 1,
+       (alreadyClicked ? -1 : 1),
+    }));
+
+        setClicked((prev) => ({
+      ...prev,
+      [emoji]: !alreadyClicked,
     }));
   };
 
   return (
     <div className="flex flex-wrap gap-3 items-center text-xl mb-6">
-      {Object.entries(reactions).map(([emoji, count]) => (
+      {/* {Object.entries(reactions).map(([emoji, count]) => ( */}
+      {emojis.map((emoji) => (
         <button
           key={emoji}
           onClick={() => handleReact(emoji)}
-          className="flex items-center gap-1 px-2 py-1 rounded hover:bg-gray-100 transition"
+          className={`flex items-center gap-1 px-2 py-1 rounded hover:bg-gray-100 transition ${
+            clicked[emoji] ? "bg-gray-100" : "hover:bg-gray-100"
+          }`}
         >
           <span>{emoji}</span>
-          <span className="text-sm text-gray-600">{count}</span>
+          {/* <span className="text-sm text-gray-600">{count}</span> */}
+                    <span className="text-sm text-gray-600">{reactions[emoji]}</span>
         </button>
       ))}
     </div>
