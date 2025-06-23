548b1b9281c0dcfc54269d26a4325957f5bcd1d8
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-06-23 22:06:48 +0200

    La rech et le tri se lancent au clic de la loupe de la barre de rech

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 function Header({ onSearch }) {
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearch = () => {
+    onSearch(searchInput);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSearch();
+    }
+  };
+
   return (
     <header
       className="h-[500px] bg-cover bg-center flex flex-col items-center justify-center text-white text-center px-4"
@@ -11,10 +27,14 @@ function Header({ onSearch }) {
         <input
           type="text"
           placeholder="Rechercher un article..."
-          onChange={(e) => onSearch(e.target.value)}
+          value={searchInput}
+          // onChange={(e) => onSearch(e.target.value)}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyDown}
           className="w-full max-w-md p-3 rounded-lg text-black shadow"
         />
         <button
+          onClick={handleSearch}
           className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
         >
           🔍
