1f58d895c8b126a5e8309ac50f4857dd7f57b121
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-07-01 03:30:53 +0200

    accès au blog si pas connecté _ blog visible, et gestion authentification uniquement pour commenter publier

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
     const saved = localStorage.getItem("blog_user");
@@ -16,6 +17,7 @@ function App() {
   const handleLogin = (userData) => {
     setUser(userData);
     localStorage.setItem("blog_user", JSON.stringify(userData));
+    setShowLogin(false);
   };
 
   const handleLogout = () => {
@@ -26,11 +28,14 @@ function App() {
 
   return (
     <>
-      {user ? (
-        <Home user={user} onLogout={handleLogout} />
-      ) : (
+      {/* {user ? ( */}
+      <Home user={user} onLogin={() => setShowLogin(true)} onLogout={handleLogout} />
+      {/* ) : ( */}
+      {showLogin && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
         <Login onLogin={handleLogin} />
-      )}
+          </div>
+    )}
     </>
   );
 }
