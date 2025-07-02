3a68c236421795618b38072fb9d7d24c4d6732b6
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-07-02 21:27:22 +0200

    nettoyage

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,24 @@ function App() {
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(false);
 
-  // useEffect(() => {
-  //   const saved = localStorage.getItem("blog_user");
-  //   if (saved) {
-  //     setUser(JSON.parse(saved));
-  //   }
-  // }, []);
-
   const handleLogin = (userData) => {
     setUser(userData);
-    // localStorage.setItem("blog_user", JSON.stringify(userData));
     setShowLogin(false);
   };
 
   const handleLogout = () => {
     setUser(null);
-    // localStorage.removeItem("blog_user");
   };
 
 
   return (
     <>
-      {/* {user ? ( */}
       <Home user={user} onLogin={() => setShowLogin(true)} onLogout={handleLogout} />
-      {/* ) : ( */}
       {showLogin && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        <Login onLogin={handleLogin} />
-          </div>
-    )}
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <Login onLogin={handleLogin} />
+        </div>
+      )}
     </>
   );
 }
