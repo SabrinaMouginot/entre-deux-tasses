052fe03611acf5037ad53bae9655434731987408
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-07-07 23:48:32 +0200

    nettoyage

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -21,6 +21,7 @@ export default function SearchBar({ searchInput, setSearchInput, onValidate }) {
         className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
       >
         🔍
+        
       </button>
     </div>
   );
